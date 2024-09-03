2c2eb92ffdb67b967792b47888e93db3ee4090dc
MRSMIRROR <125119475+MRSMIRROR@users.noreply.github.com>
2024-09-03 15:53:21 +0700

    commit 3.2

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -71,7 +71,14 @@ public class EventController {
         for (int i = firstIndex; i < firstIndex + perPage; i++) {
             output.add(eventList.get(i));
         }
-        return ResponseEntity.ok(output);
+        try {
+            for (int i = firstIndex; i < firstIndex + perPage; i++) {
+                output.add(eventList.get(i));
+            }
+            return ResponseEntity.ok(output);
+        } catch (IndexOutOfBoundsException ex) {
+            return ResponseEntity.ok(output);
+        }
     }
 }
 
